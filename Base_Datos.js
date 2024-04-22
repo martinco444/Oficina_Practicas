e30cc4eb948e80beb3dd722a00a742a4ee1723cb
Martín Correa <comartin910@gmail.com>
2024-04-22 13:56:09 -0500

    base de datos inicializada sin funcionalidad

--- Base_Datos.js
+++ Base_Datos.js
@@ -1,7 +1,36 @@
 var bd;
 
-function iniciarBaseDatos(){
-    indexedDB.open("Datos_Estudiantes_practicas");
+function IniciarBaseDatos(){
+   var solicitud = indexedDB.open("Datos_Estudiantes_practicas");
+
+
+   solicitud.addEventListener("error", MostrarError);
+   
+   solicitud.addEventListener("success", Comenzar);
+   
+   solicitud.addEventListener("upgradeneeded", CrearAlmacen);
 }
 
-window.addEventListener("load", iniciarBaseDatos);
+
+function MostrarError(evento){
+
+    alert ("Error presentado: " + evento.code + " / " + evento.message);
+
+}
+
+function Comenzar(evento){
+
+    bd = evento.target.result;
+    console.log("funcion comenzar");
+}
+
+function CrearAlmacen(evento){
+
+    var baseDatos = evento.target.result;
+    var almacen = baseDatos.createObjectStore("Estudiantes", {keyPath: "codigo"});
+    almacen.createIndex("BuscarNombre", "nombre", {unique: false});
+    console.log("funcion crear almacen");
+}
+
+
+window.addEventListener("load", IniciarBaseDatos);
